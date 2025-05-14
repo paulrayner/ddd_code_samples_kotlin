7ad056ab0cf4237388385f991fcd8c56db025e9f
Chris Simon <chris@devcycles.io>
2025-05-15 07:54:39 +1000

    Add Adjudication explanation comment

--- src/main/kotlin/ClaimsAdjudication.kt
+++ src/main/kotlin/ClaimsAdjudication.kt
@@ -1,5 +1,14 @@
 package warranty
 
+/**
+ * Adjudicate/adjudication - a judgment made on a claim to determine whether
+ * we are legally obligated to process the claim against the warranty. From
+ * Wikipedia (https://en.wikipedia.org/wiki/Adjudication):
+ * "Claims adjudication" is a phrase used in the insurance industry to refer to
+ * the process of paying claims submitted or denying them after comparing claims
+ * to the benefit or coverage requirements.
+ */
+
 class ClaimsAdjudication {
     fun adjudicate(contract: Contract, newClaim: Claim) {
         val claimTotal = contract.claims.sumOf { it.amount }
